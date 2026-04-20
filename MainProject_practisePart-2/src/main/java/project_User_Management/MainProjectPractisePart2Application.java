a9e5216dd1e6c90175bccf1d31b576342cfefd4c
Govind2911 <govindpatidar4214@gmail.com>
2026-04-20 10:16:19 +0530

    Project was set up

--- MainProject_practisePart-2/src/main/java/project_User_Management/MainProjectPractisePart2Application.java
+++ MainProject_practisePart-2/src/main/java/project_User_Management/MainProjectPractisePart2Application.java
@@ -8,6 +8,7 @@ public class MainProjectPractisePart2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainProjectPractisePart2Application.class, args);
+		System.out.println("working fine");
 	}
-
+       
 }
